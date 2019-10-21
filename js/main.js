9cfead82bea692f3b990615fca6f6dd1dfc993c2
kareem El-Giushy <kareem.64123@gmail.com>
2019-10-21 15:49:37 +0200

    The First two sections and the layout

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+  // Declaring Vars
+  var screenH = $(window).height();
+  var screenW = $(window).width();
+  // the NavBar
+  var navHeight = $(".navy").height();
+  // Global
+  $("section").height(screenH);
+  // Functions
+
+});
